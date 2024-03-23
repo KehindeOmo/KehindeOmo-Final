8ba2131edace8047a26435b9d8faae04d5664f0a
KehindeOmo <111716099+KehindeOmo@users.noreply.github.com>
2024-03-23 13:23:05 -0500

    Add files via upload

--- webdevelopment/newScript.js
+++ webdevelopment/newScript.js
@@ -1,3 +1,11 @@
 function changeAvatar(){
     document.getElementById("ofcourse").src= "newEmoji.png"
 }
+function newNav(){
+    var navMenus=document.getElementById("navMenus");
+    if(navMenus.style.display=== "none"){
+        navMenus.style.display= "block";
+    }else{
+        navMenus.style.display="none";
+    }
+}
