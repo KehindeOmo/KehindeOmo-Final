c6da2ca22d558788184737fb71c2116624e12b94
KehindeOmo <111716099+KehindeOmo@users.noreply.github.com>
2024-04-04 00:11:24 -0500

    Add files via upload

--- webdevelopment/newScript.js
+++ webdevelopment/newScript.js
@@ -1,6 +1,16 @@
-function changeAvatar(){
-    document.getElementById("ofcourse").src= "newEmoji.png"
-}
+const chAvatar=document.getElementById('ofcourse');
+let chAvatar1=true;
+chAvatar.addEventListener('click', function(){
+    if(chAvatar1){
+        ofcourse.src= "kehindeEmoji.png"
+    }else{
+        ofcourse.src="newEmoji.png"
+    }
+    chAvatar1= !chAvatar1;
+});
+//function changeAvatar(){
+   // document.getElementById("ofcourse").src= "newEmoji.png"
+//}
 function newNav(){
     var navMenus=document.getElementById("navMenus");
     if(navMenus.style.display=== "none"){
@@ -8,4 +18,10 @@ function newNav(){
     }else{
         navMenus.style.display="none";
     }
+}
+
+function darkMode(){
+
+    var body = document.body;
+    body.classList.toggle("dark-mode");
 }
